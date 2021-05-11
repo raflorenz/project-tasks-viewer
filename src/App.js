89f497cb5383b51ef0d284565d2a36c7f5cb5fe4
Raffy Lorenzana <rjlorenz931@gmail.com>
2021-05-11 18:14:33 +0800

    add project files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+const tasksData = [
+  {
+    id: 1,
+    name: 'Task #1',
+    description: 'Lorem ipsum dolor sit amet',
+    dueDate: 'May 21',
+    assignee: 'Raf Lorenz',
+    list: 'Current Sprint'
+  },
+  {
+    id: 2,
+    name: 'Task #2',
+    description: 'Lorem ipsum dolor sit amet',
+    dueDate: 'May 21',
+    assignee: 'Raf Lorenz',
+    list: 'In Progress'
+  },
+  {
+    id: 3,
+    name: 'Task #3',
+    description: 'Lorem ipsum dolor sit amet',
+    dueDate: 'May 21',
+    assignee: 'Raf Lorenz',
+    list: 'QA'
+  },
+  {
+    id: 4,
+    name: 'Task #4',
+    description: 'Lorem ipsum dolor sit amet',
+    dueDate: 'May 21',
+    assignee: 'Raf Lorenz',
+    list: 'Resolved'
+  },
+  {
+    id: 5,
+    name: 'Task #5',
+    description: 'Lorem ipsum dolor sit amet',
+    dueDate: 'May 21',
+    assignee: 'Raf Lorenz',
+    list: 'Completed'
+  }
+];
+
+let latestID = 5;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Project Name</h1>
+      <div className="task-list-wrapper">
+        <TaskList taskList="Current Sprint" />
+        <TaskList taskList="In Progress" />
+        <TaskList taskList="QA" />
+        <TaskList taskList="Resolved" />
+        <TaskList taskList="Completed" />
+      </div>
+    </div>
+  );
+}
+
+function TaskList({ taskList }) {
+  const [tasks, setTasks] = useState(tasksData);
+
+  const addTask = () => {
+    latestID++;
+    setTasks([
+      ...tasks,
+      {
+        id: latestID,
+        name: `Task #${latestID}`,
+        description: 'Lorem ipsum dolor sit amet',
+        dueDate: 'May 21',
+        assignee: 'Raf Lorenz',
+        list: taskList
+      }
+    ]);
+  };
+
+  return (
+    <div className="task-list">
+      <h3>{taskList}</h3>
+      {tasks.map(task => task.list === taskList && <TasktItem key={task.id} {...task} />)}
+      <button onClick={() => addTask()}>Add a task</button>
+    </div>
+  );
+}
+
+function TasktItem({ name, description, dueDate, assignee }) {
+  return (
+    <div className="task-list-item">
+      <p>Task name: {name}</p>
+      <p>Task description: {description}</p>
+      <p>Task due date: {dueDate}</p>
+      <p>Task assignee: {assignee}</p>
     </div>
   );
 }
